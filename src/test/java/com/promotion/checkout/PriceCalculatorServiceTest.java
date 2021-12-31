489daa7e0e0b0f887d8dc5ee2a077f3666ffc42a
Keerthivag <keerthi.vasudevan@mnscorp.net>
2021-12-31 13:25:11 +0000

    Added PriceCalculatorService class

---
+++ src/test/java/com/promotion/checkout/PriceCalculatorServiceTest.java
@@ -0,0 +1,21 @@
+package com.promotion.checkout;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PriceCalculatorServiceTest {
+	
+	PriceCalculatorService priceCalculatorService;
+	
+	@Before
+	public void setup() {
+		priceCalculatorService = new PriceCalculatorServiceImpl();
+	}
+	
+	@Test
+	public void testCalculateProductPrice() {
+		
+	}
+	
+
+}
