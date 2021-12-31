dea64b9f9161488d895555ad887b9a5da891419e
Keerthivag <keerthi.vasudevan@mnscorp.net>
2021-12-31 16:14:16 +0000

    Test cases for skuid A promotion

--- src/test/java/com/promotion/checkout/PriceCalculatorServiceTest.java
+++ src/test/java/com/promotion/checkout/PriceCalculatorServiceTest.java
@@ -8,27 +8,42 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.promotion.checkout.domain.Product;
 import com.promotion.checkout.exception.ProductEmptyException;
 
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class PriceCalculatorServiceTest {
 
-	PriceCalculatorService priceCalculatorService;
+	private PriceCalculatorService priceCalculatorService;
+
+	private Product productA;
+	private Product productB;
+	private Product productC;
+	private Product productD;
 
 	@Before
 	public void setup() {
 		priceCalculatorService = new PriceCalculatorServiceImpl();
+		productA = new Product("A", 50);
+		productB = new Product("B", 30);
+		productC = new Product("C", 20);
+		productD = new Product("D", 15);
 	}
 
 	@Test
 	public void testCalculateProductPriceWhenValidProducts() {
-		Product productA = new Product("A", 50);
-		Product productB = new Product("B", 30);
-		Product productC = new Product("C", 20);
-		Product productD = new Product("D", 15);
 		List<Product> products = new ArrayList<>(Arrays.asList(productA, productB, productC, productD));
 		Assert.assertEquals(110, priceCalculatorService.calculateProductPrice(products));
+	}
+
+	@Test
+	public void testCalculateProductPriceWhenPromotionAppliedForSkuIDA() {
+		List<Product> products = new ArrayList<>(Arrays.asList(productA, productA, productA, productB, productC));
+		Assert.assertEquals(180, priceCalculatorService.calculateProductPrice(products));
 	}
 
 	@Test(expected = ProductEmptyException.class)
