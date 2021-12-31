0ddec8ceccebd6005a485ed43ef5be7726500e98
Keerthivag <keerthi.vasudevan@mnscorp.net>
2021-12-31 15:12:04 +0000

    Refactored the code

--- src/main/java/com/promotion/checkout/PriceCalculatorServiceImpl.java
+++ src/main/java/com/promotion/checkout/PriceCalculatorServiceImpl.java
@@ -14,46 +14,67 @@ public class PriceCalculatorServiceImpl implements PriceCalculatorService {
 	@Override
 	public int calculateProductPrice(List<Product> products) {
 		AtomicInteger totalPrice = new AtomicInteger(0);
-		if (null == products || products.isEmpty()) {
-			throw new ProductEmptyException("Product List is empty");
-		} else {
-			String skuIdC = ProductPrice.C.name();
-			String skuIdD = ProductPrice.D.name();
-
-			int skuIdOfC = (int) products.stream().filter(prod -> prod.getSkuId().equalsIgnoreCase(skuIdC)).count();
-			int skuIdOfD = (int) products.stream().filter(prod -> prod.getSkuId().equalsIgnoreCase(skuIdD)).count();
-
-			if (skuIdOfC == skuIdOfD) {
-				int promotionPrice = PromotionPrice.CD.getPrice();
-				totalPrice.getAndAdd(skuIdOfD * promotionPrice);
+		try {
+			if (null == products || products.isEmpty()) {
+				throw new ProductEmptyException("Product List is empty");
 			} else {
-				int minCOrD = Math.min(skuIdOfC, skuIdOfD);
-				int promotionPrice = PromotionPrice.CD.getPrice();
-				totalPrice.getAndAdd(minCOrD * promotionPrice);
-				if (skuIdOfC > skuIdOfD) {
-					int prodPrice = ProductPrice.valueOf(skuIdC).getPrice();
-					totalPrice.getAndAdd((skuIdOfC - skuIdOfD) * prodPrice);
-				} else {
-					int prodPrice = ProductPrice.valueOf(skuIdD).getPrice();
-					totalPrice.getAndAdd((skuIdOfD - skuIdOfC) * prodPrice);
-				}
-			}
-			products.removeIf(
-					(Product p) -> p.getSkuId().equalsIgnoreCase(skuIdC) || p.getSkuId().equalsIgnoreCase(skuIdD));
+				totalPrice.addAndGet(applypromotionPriceForCAndD(products));
 
-			products.stream().collect(Collectors.groupingBy(Product::getSkuId)).forEach((skuId, prods) -> {
-				int skuIdQty = prods.size();
-				if (null != PromotionPrice.valueOf(skuId) && null != ProductPrice.valueOf(skuId)) {
-					int prodPrice = ProductPrice.valueOf(skuId).getPrice();
-					int totalQty = PromotionPrice.valueOf(skuId).getQty();
-					int offerprice = PromotionPrice.valueOf(skuId).getPrice();
-					totalPrice.getAndAdd((skuIdQty / totalQty) * offerprice + (skuIdQty % totalQty * prodPrice));
-				} else {
-					totalPrice.getAndAdd(products.stream().collect(Collectors.summingInt(Product::getPrice)));
-				}
-			});
+				products.stream().collect(Collectors.groupingBy(Product::getSkuId)).forEach((skuId, prods) -> {
+					totalPrice.addAndGet(applypromotionPriceForAorB(skuId, prods));
+				});
+			}
+		} catch (Exception ex) {
+			throw new ProductEmptyException("Arithmetic error occured :: " + ex);
 		}
 		return totalPrice.get();
+	}
+
+	private int applypromotionPriceForAorB(String skuId, List<Product> products) {
+		int totalPrice = 0;
+		int skuIdQty = products.size();
+		if (null != PromotionPrice.valueOf(skuId) && null != ProductPrice.valueOf(skuId)) {
+			int prodPrice = ProductPrice.valueOf(skuId).getPrice();
+			int totalQty = PromotionPrice.valueOf(skuId).getQty();
+			int offerprice = PromotionPrice.valueOf(skuId).getPrice();
+			totalPrice = (skuIdQty / totalQty) * offerprice + (skuIdQty % totalQty * prodPrice);
+		} else {
+			totalPrice = products.stream().collect(Collectors.summingInt(Product::getPrice));
+		}
+		return totalPrice;
+	}
+
+	private int applypromotionPriceForCAndD(List<Product> products) {
+
+		int totalPrice = 0;
+		String skuIdC = ProductPrice.C.name();
+		String skuIdD = ProductPrice.D.name();
+
+		long skuIdOfC = products.stream().filter(prod -> prod.getSkuId().equalsIgnoreCase(skuIdC)).count();
+		long skuIdOfD = products.stream().filter(prod -> prod.getSkuId().equalsIgnoreCase(skuIdD)).count();
+
+		if (skuIdOfC == skuIdOfD) {
+			int promotionPrice = PromotionPrice.CD.getPrice();
+			totalPrice += skuIdOfD * promotionPrice;
+		} else {
+			long minCOrD = Math.min(skuIdOfC, skuIdOfD);
+			int promotionPrice = PromotionPrice.CD.getPrice();
+			totalPrice += minCOrD * promotionPrice;
+			if (skuIdOfC > skuIdOfD) {
+				totalPrice += calculatePriceForCAndD(skuIdC, skuIdOfC, skuIdOfD);
+			} else {
+				totalPrice += calculatePriceForCAndD(skuIdD, skuIdOfD, skuIdOfC);
+			}
+		}
+		products.removeIf(
+				(Product p) -> p.getSkuId().equalsIgnoreCase(skuIdC) || p.getSkuId().equalsIgnoreCase(skuIdD));
+		return totalPrice;
+
+	}
+
+	private long calculatePriceForCAndD(String sku, long sku1, long sku2) {
+		int prodPrice = ProductPrice.valueOf(sku).getPrice();
+		return (sku1 - sku2) * prodPrice;
 	}
 
 }
